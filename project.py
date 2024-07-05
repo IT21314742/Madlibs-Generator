b824c9e33158b6eaf6d9eea949e07fbdb1655f06
IT21314742 <it21314742@my.sliit.lk>
2024-07-05 20:56:10 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -15,7 +15,7 @@ for i, char in enumerate(story):
     if char == target_start:
         start_of_word = i
     
-    if  == target_end and start_of_word != -1:
+    if char == target_end and start_of_word != -1:
         word = story[start_of_word: i + 1]
         words.add(word)
         start_of_word = -1
